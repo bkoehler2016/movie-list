4581e6c89988d20fe87bd29b1bd156448248d14b
Ben Koehler <bkoehler85282@gmail.com>
2020-04-12 19:00:53 -0700

    added a method to add movies with a form

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import MoviesList from "./components/MoviesList";
+import MoviesForm from "./components/MoviesForm";
 
 function App() {
   return (
     <div className="App">
       <h1>Family Movie List</h1>
       <MoviesList />
+      <MoviesForm />
     </div>
   );
 }
---
+++ client/src/components/MoviesForm.js
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Button } from "reactstrap";
+
+export default function MoviesForm() {
+  const [newMovie, setNewMovie] = useState({});
+
+  const handleChange = (event) => {
+    setNewMovie({ ...newMovie, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = () => {
+    axios.post("http://localhost:5002/api/movies", newMovie).then((res) => {
+      console.log(res);
+    });
+  };
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <input
+        type="text"
+        name="owner"
+        id="owner"
+        value={newMovie.owner}
+        placeholder="Owner"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="title"
+        id="title"
+        value={newMovie.title}
+        placeholder="title"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="release_year"
+        id="release_year"
+        value={newMovie.release_year}
+        placeholder="release year"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="rating"
+        id="rating"
+        value={newMovie.rating}
+        placeholder="rating"
+        onChange={handleChange}
+      />
+      <input
+        type="text"
+        name="format"
+        id="format"
+        value={newMovie.format}
+        placeholder="format"
+        onChange={handleChange}
+      />
+      <Button color="primary" type="submit">
+        Add New Movie
+      </Button>
+    </form>
+  );
+}
