b98691e015b37ae67e5641aa60361e501dfa2a30
Ben Koehler <bkoehler85282@gmail.com>
2020-04-13 13:37:58 -0700

    updated node

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const server = require("./api/server.js");
 
-const port = process.env.port || 5002;
-server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+const port = process.env.PORT;
+server.listen(port, () =>
+  console.log(`Server started on http://localhost:${port}`)
+);
