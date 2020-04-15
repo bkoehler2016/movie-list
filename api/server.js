f07488fe868d22ab0d78c934f394ec08db50bd3c
Ben Koehler <bkoehler85282@gmail.com>
2020-04-14 19:23:03 -0700

    added proxy service

--- api/server.js
+++ api/server.js
@@ -3,7 +3,12 @@ const express = require("express");
 const helmet = require("helmet");
 
 const moviesRouter = require("../movies/movies-router.js");
+const proxy = require("http-proxy-middleware");
 
+module.exports = function (app) {
+  // add other server routes to path array
+  app.use(proxy(["/api"], { target: "http://localhost:5002" }));
+};
 const server = express();
 const cors = require("cors");
 
